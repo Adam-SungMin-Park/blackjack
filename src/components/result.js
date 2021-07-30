58b0ab50c5397c5c8d14325c756e934d245493c3
Adam SungMin Park <parksm2@uci.edu>
2021-07-30 22:18:28 +0000

    logic / design

---
+++ src/components/result.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function Result({playerScore, dealerScore}) {
+  console.log(playerScore);
+  console.log(dealerScore);
+
+
+
+   if(dealerScore > 21 && playerScore <=21){
+    return(
+      <div className ="result">
+        Player Wins
+      </div>
+    )
+  }
+  else if(dealerScore <= 21 && playerScore >21){
+    return(
+      <div className ="result">
+        Dealer Wins
+      </div>
+    )
+  }
+  else if(dealerScore === 21 && playerScore ===21){
+    return(
+      <div className ="result">
+        Tie
+      </div>
+    )
+
+  }
+   else{
+    return(
+      <div className ="result">
+        game ongoing
+      </div>
+    )
+  }
+
+
+}
