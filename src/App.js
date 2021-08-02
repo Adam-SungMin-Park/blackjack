f9b1087bacac97f7dc818fc85e9d773d3320a3a3
Adam SungMin Park <parksm2@uci.edu>
2021-08-02 05:37:57 +0000

    no duplicate

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect , useState } from 'react';
 
 
 
-function App() {
+export default function App() {
   const [playerCard , setPlayerCard ] = useState();
   const [demo , setDemo] = useState(false)
   const [dealerCard , setDealerCard ] = useState();
@@ -14,6 +14,7 @@ function App() {
   const [dealerScore , setDealerScore ] = useState(0);
   const [deck , setDeck] = useState(['loading'])
   const [winner , setWinner] = useState()
+
   useEffect(()=>{
     const suits = ["spades", "diamonds", "clubs", "hearts"];
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -21,24 +22,28 @@ function App() {
 
     for(let i = 0 ; i < suits.length ; i++){
       for(let j = 0 ; j < values.length ; j++){
-          let card = {value: values[j] , suit : suits[i]};
+          let card = {value: values[j] , suit : suits[i] , score : values[j]};
+          if(values[j]==="Q" ||values[j]==="J" ||values[j]==="K" ){
+            card.score = 10;
+          }
           deck.push(card);
       }
       setDeck(deck);
     }
-    let random = Math.floor((Math.random()*deck.length));
-    let randomDealer = Math.floor((Math.random()*deck.length));
-    let random2 =  Math.floor((Math.random()*deck.length));
-    if(random === randomDealer || random === random2 || random2 === randomDealer){
-        random = Math.floor((Math.random()*deck.length));
-        random2 =  Math.floor((Math.random()*deck.length));
+    for (let i = deck.length -1 ; i > 0 ; i--){
+      let j = Math.floor(Math.random()*i);
+      let temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
     }
+    setPlayerCard([deck[deck.length-1],deck[deck.length-2]]);
+    setDealerCard([deck[deck.length-3]]);
+    deck.pop()
+    deck.pop()
+    deck.pop()
 
-    setPlayerCard([deck[random],deck[random2]]);
-    setDealerCard([deck[randomDealer]]);
-    deck.splice(randomDealer,1)
-    deck.splice(random2,1);
-    deck.splice(random,1);
+
+    console.log(deck)
 
   },[])
   useEffect(()=>{
@@ -46,7 +51,7 @@ function App() {
     let dealerScore =0;
     if(playerCard !== undefined ){
       for(let i = 0 ; i < playerCard.length ; i++){
-        if(playerCard[i].value==="A"){
+        if(playerCard[i].score==="A"){
           if(score >= 11){
              playerCard[i].score = 1;
 
@@ -54,14 +59,10 @@ function App() {
             playerCard[i].score = 11;
           }
         }
-        else if(playerCard[i].value==="Q" ||playerCard[i].value==="J" ||playerCard[i].value==="K" ){
-          playerCard[i].score = 10;
-        }
-        else{
-          playerCard[i].score = playerCard[i].value
-        }
         score = score + +playerCard[i].score
+
       }
+
       setPlayerScore(score)
     }
     if(dealerCard !== undefined ){
@@ -73,12 +74,6 @@ function App() {
             dealerCard[i].score = 11;
           }
         }
-        else if(dealerCard[i].value==="Q" ||dealerCard[i].value==="J" ||dealerCard[i].value==="K" ){
-          dealerCard[i].score = 10;
-        }
-        else{
-          dealerCard[i].score = dealerCard[i].value;
-        }
           dealerScore = dealerScore + +dealerCard[i].score
       }
       setDealerScore(dealerScore)
@@ -87,47 +82,50 @@ function App() {
 
   function hit(){
     console.log(deck)
-    let random = Math.floor((Math.random()*deck.length));
-    let randomDealer = Math.floor((Math.random()*deck.length));
-    if(random===randomDealer){
-      random++;
-    }
+
     if(playerScore <21 &&dealerScore <17){
-      setDealerCard([...dealerCard,deck[random]]);
-      deck.splice(random,1);
-      setPlayerCard([...playerCard,deck[randomDealer]]);
-      deck.splice(randomDealer,1);
+      setDealerCard([...dealerCard,deck[deck.length-1]]);
+      deck.pop()
+      setPlayerCard([...playerCard,deck[deck.length-1]]);
+      deck.pop()
     }
     if(playerScore < 21 && dealerScore >=17){
-      setPlayerCard([...playerCard,deck[randomDealer]]);
-      deck.splice(randomDealer,1);
-    }
+      setPlayerCard([...playerCard,deck[deck.length-1]]);
+    deck.pop()
+
   }
+}
   function reset(){
+
     const suits = ["spades", "diamonds", "clubs", "hearts"];
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     const deck = [];
+
     for(let i = 0 ; i < suits.length ; i++){
       for(let j = 0 ; j < values.length ; j++){
-          let card = {value: values[j] , suit : suits[i]};
+          let card = {value: values[j] , suit : suits[i] , score : values[j]};
+          if(values[j]==="Q" ||values[j]==="J" ||values[j]==="K" ){
+            card.score = 10;
+          }
           deck.push(card);
       }
       setDeck(deck);
     }
     setWinner()
-    let random = Math.floor((Math.random()*deck.length));
-    let randomDealer = Math.floor((Math.random()*deck.length));
-    let random2 = Math.floor((Math.random()*deck.length));
-    if(random === randomDealer || random === random2 || randomDealer === random2){
-        random = Math.floor((Math.random()*deck.length));
-        random2 = Math.floor((Math.random()*deck.length));
+   for (let i = deck.length -1 ; i > 0 ; i--){
+      let j = Math.floor(Math.random()*i);
+      let temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
     }
-    setPlayerCard([deck[random],deck[random2]]);
-    setDealerCard([deck[randomDealer]]);
-    deck.splice(randomDealer,1)
-    deck.splice(random,1);
+    setPlayerCard([deck[deck.length-1],deck[deck.length-2]]);
+    setDealerCard([deck[deck.length-3]]);
+    deck.pop()
+    deck.pop()
+    deck.pop()
   }
   function stay(){
+
     if(dealerScore >= 17 && playerScore > dealerScore){
       setWinner("player");
     }
@@ -143,6 +141,7 @@ function App() {
       deck.splice(random, 1);
     }
   }
+
   if(demo ===false){
     return(
       <div className = "App">
@@ -150,7 +149,9 @@ function App() {
           <div className = "landingPage">
             <div>Thank you for visiting. This is a Black Jack web application.</div>
             <div>Rules:</div>
-            <div>1. Reset will give new cards to both dealer and the player.</div>
+            <div>1. "Reset" will give new cards to both dealer and the player.</div>
+            <div>2. "Stay" will give new card to dealer as long as dealer's score is less than 17.</div>
+            <div>3. "Hit" will give card to player. Dealer will get a card if dealer's score is less than 17.</div>
             <button onClick ={()=>setDemo(true)}>Play the Game</button>
           </div>
         </div>
@@ -184,7 +185,7 @@ function App() {
       </div>
     );
   }
-  else if(dealerCard.length === playerCard.length+1 ) {
+  else if(dealerCard.length === playerCard.length+1 || deck.length <3 ) {
     return(
        <div className="App">
         <div className="container">
@@ -225,6 +226,7 @@ function App() {
             card = {playerCard}
             score = {playerScore}
           />
+
           <div className = "gameOptions">
             <button onClick = {reset } >Reset</button>
           </div>
@@ -236,6 +238,4 @@ function App() {
       </div>
     )
   }
-}
-
-export default App;
+};
